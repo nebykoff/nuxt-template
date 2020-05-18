72a64a8012f6cdce778d29dcf35b07652d18ca05
Anton Nebykov <an@idaproject.com>
2020-05-18 18:06:29 +0700

    Добавил отображение названия кошки и ее свойств

--- components/common/Gallery/GalleryItem.vue
+++ components/common/Gallery/GalleryItem.vue
@@ -1,6 +1,14 @@
 <template>
     <div :class="$style.GalleryItem">
         <img :src="item.img" alt="">
+        <div :class="$style.text">
+            <div :class="$style.title">{{ item.title }}</div>
+            <div :class="$style.tags">
+                <span v-for="(tag, index) of item.tags" :key="tag">
+                    {{ tag.toLowerCase() }}<span v-if="index < item.tags.length - 1">,</span>
+                </span>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -18,6 +26,22 @@
 
 <style module lang="scss">
     .GalleryItem {
-        //
+        position: relative;
+
+        &:hover {
+            .text {
+                display: block;
+            }
+        }
+    }
+
+    .text {
+        position: absolute;
+        bottom: 0;
+        display: none;
+        width: 100%;
+        height: 90px;
+        padding: 18px 22px;
+        background-color: $clr-gray;
     }
 </style>
