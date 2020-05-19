a8dcb7369f9d8ef5c40b172d84d9bdafa25686a2
Anton Nebykov <an@idaproject.com>
2020-05-19 14:20:10 +0700

    Добавил сайдбар

---
+++ components/layout/TheSidebar.vue
@@ -0,0 +1,17 @@
+<template>
+    <aside :class="$style.TheSidebar">
+        <slot></slot>
+    </aside>
+</template>
+
+<script>
+    export default {
+        name: 'TheSidebar'
+    };
+</script>
+
+<style module lang="scss">
+    .TheSidebar {
+        width: 200px;
+    }
+</style>
--- pages/cat/_id.vue
+++ pages/cat/_id.vue
@@ -1,17 +1,28 @@
 <template>
     <div :class="$style.CatDetail">
         <Breadcrumbs :breadcrumbs="breadcrumbs"/>
-        {{ cat }}
+        <div :class="$style.CatDetail__wrapper">
+            <section :class="$style.CatDetail__content">
+                {{ cat }}
+            </section>
+            <TheSidebar :class="$style.CatDetail__sidebar">
+                <div>БЛОК 1</div>
+                <div>БЛОК 2</div>
+                <div>БЛОК 3</div>
+            </TheSidebar>
+        </div>
     </div>
 </template>
 
 <script>
     import Breadcrumbs from '~/components/common/Breadcrumbs';
+    import TheSidebar from '~/components/layout/TheSidebar';
 
     export default {
         name: 'CatDetail',
 
         components: {
+            TheSidebar,
             Breadcrumbs,
         },
 
@@ -28,6 +39,13 @@
 
 <style module lang="scss">
     .CatDetail {
-        //
+        &__wrapper {
+            display: flex;
+            justify-content: space-between;
+        }
+
+        &__content {
+            flex-grow: 1;
+        }
     }
 </style>
